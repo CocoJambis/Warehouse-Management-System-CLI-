e688a4df11456eb1ee20651f7c64199606f1dca8
Matteo <matteo.montalbotti@gmail.com>
2026-09-22 16:25:58 +0200

    Added pydantic model and test fastapi

--- db.py
+++ db.py
@@ -1,18 +1,20 @@
 from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
+from pydantic import BaseModel
 
 engine = create_engine('sqlite:///database.db')
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
 session = Session()
 
-class BaseModel(Base):
+#Sqlalchemy models
+class BaseModello(Base):
     __abstract__ = True
     __allow_unmapped__ = True
 
     id = Column(Integer, primary_key=True)
 
-class Magazzino(BaseModel):
+class Magazzino(BaseModello):
     __tablename__ = 'magazzino'
 
     id = Column(Integer, primary_key=True)
@@ -21,7 +23,7 @@ class Magazzino(BaseModel):
     quantity = Column(Integer)
 
 
-class Item(BaseModel):
+class Item(BaseModello):
     __tablename__ = 'items'
 
     id = Column(Integer, primary_key=True)
@@ -29,3 +31,33 @@ class Item(BaseModel):
     name = Column(String, nullable=False)
 
 Base.metadata.create_all(engine)
+
+#Pydantic models Item
+class ItemBase(BaseModel):
+    code:str
+    name:str
+
+class ItemCreate(ItemBase):
+    pass
+
+class Item(ItemBase):
+    id:int
+
+    class Config:
+        orm_mode = True
+
+
+#Pydantic models Magazzino
+class MagazzinoBase(BaseModel):
+    code:str
+    name:str
+    quantity:int
+
+class MagazzinoCreate(MagazzinoBase):
+    pass
+
+class Magazzino(MagazzinoBase):
+    id:int
+
+    class Config:
+        orm_mode = True
