3bb4c5d3cff28f002786ab6f6608bc4e88a5c645
Matteo <matteo.montalbotti@gmail.com>
2026-09-23 15:02:04 +0200

    Tolto sqlite3 e aggiunto postgresql

--- api_version/db.py
+++ api_version/db.py
@@ -1,7 +1,8 @@
 from sqlalchemy import  create_engine
 from sqlalchemy.orm import  sessionmaker
 
-engine = create_engine('sqlite:///database.db')
+engine = create_engine('postgresql://postgres:1312@localhost:5432/Magazzino')
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 #Connessione al Database
